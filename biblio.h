60cc5cd3ee9ede44d63baf7cd6e6aa97502751a2
Zerkhef Abd Raouf <112574242+raufzer@users.noreply.github.com>
2022-12-24 20:26:45 +0100

    Create biblio.h

---
+++ biblio.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+
+extern bool verif3(char [100]);
+extern bool verif2(int);
+extern bool verif(int, int);
+extern int b2versb10(int);
+extern int b10versb2(int);
+extern int b2versb8(int);
+extern int b10versb8(int);
+extern int b8versb10(n8);
+extern int b8versb2(s8);
+extern void b10versb16(int n10);
+extern void b2versb16(n2);
+extern void b16versb10 (char hex[100]);
+extern void b16versb2(char hex[100]);
+extern void b16versb8 (char hex[100]);
+
